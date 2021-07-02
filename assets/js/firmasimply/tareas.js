eac06e6194e6c74ef558c93494ce2151dd27d86a
MAUV90 <urban_a_90@hotmail.com>
2021-07-02 13:43:48 +0200

    cambio Oscar

--- assets/js/firmasimply/tareas.js
+++ assets/js/firmasimply/tareas.js
@@ -3,7 +3,31 @@ import Tarea from './Modules/Tarea.js';
 import Categoria from './Modules/Categoria.js';
 
 // Funcionalidad mostrar listado de tareas
+getListadoTareas();
+async function getListadoTareas() {
+    let listadoTareas = await Pildora.getListadoTareas();
+    console.log(listadoPildoras);
+    const list = document.getElementById('listado')
+    // books.forEach((book) => UI.addBookToList(book))
+    //let finales = listado.reverse();
+    for (let i = 0; i < listadoPildoras.length; i++) {
+        if (i > 0 & i == 6) {
+            const row = document.createElement('tr')
 
+            row.innerHTML = `
+                            <td>${listadoPildoras[i].id}</td>
+                            <td>${listadoPildoras[i].nombre}</td>
+                            <td>${listadoPildoras[i].descripcion}</td>
+                            <td>${listadoPildoras[i].fecha_presentacion}</td>
+                            <td>${listadoPildoras[i].created_at}</td>
+                            <td>${listadoPildoras[i].estado}</td>
+                            <td><button onclick="borrarPildora()">Borrar</button></td>
+                            `
+
+            list.appendChild(row)
+        }
+    }
+}
 // Funcionalidad crear una tarea
 
 // Funcionalidad de borrar una tarea
