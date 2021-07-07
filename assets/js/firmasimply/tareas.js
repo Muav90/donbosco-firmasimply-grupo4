4a77eae3163f08dfa1fd8e9616fcd06ec019267b
oscardg77 <82647316+oscardg77@users.noreply.github.com>
2021-07-07 15:25:58 +0200

    subir main

--- assets/js/firmasimply/tareas.js
+++ assets/js/firmasimply/tareas.js
@@ -3,49 +3,6 @@ import Tarea from './Modules/Tarea.js';
 import Categoria from './Modules/Categoria.js';
 
 // Funcionalidad mostrar listado de tareas
-getListadoTarea();
-async function getListadoTarea() {
-    let listadoTareas = await Tarea.getListadoTareas();
-    console.log(listadoTareas);
-    const list = document.getElementById('lista')
-    
-    for (let i = 0; i < listadoTareas.length; i++) {
-        if (i > 0 ) {
-            const row = document.createElement('tr')
-
-            row.innerHTML = `
-            
-                            <td>${listadoTareas[i].titulo}</td>
-                            <td>${listadoTareas[i].categoria_id}</td>
-                            <td>${listadoTareas[i].descripcion}</td>
-                            <td>${listadoTareas[i].created_at}</td>
-                            <td>${listadoTareas[i].estado}</td>
-                            <td><button onclick="borrarTarea()">Borrar</button></td>
-                            `
-
-            list.appendChild(row)
-        }
-    }
-}
-// Consultar el listado de píldoras
-Pildora.getListadoTarea();
-// Funcionalidad crear píldora
-
-var form = document.getElementById('form');
-async function crearTarea(e) {
-    e.preventDefault();
-    var crearTarea = {
-        nombre: document.getElementById('fname').value,
-        descripcion: document.getElementById('lname').value,
-        fecha_presentacion: document.getElementById('date').value,
-        estado: 0, // 0 pendiente, 1 presentada
-        user_id: Auth.getCoder().id, // esta funcion devuelve el id del coder logeado
-    }
-    await Pildora.crearPildora(crearPildora);
-    window.location.reload();
-};
-
-form.addEventListener('submit', crearPildoras)
 
 // Funcionalidad crear una tarea
 
